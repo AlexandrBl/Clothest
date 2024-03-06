ab1cd05019d6628a80d56bedee27ed7ad0540694
icy1xtee <easton2904@mail.ru>
2024-03-07 02:13:08 +0300

    fix: consolelogs removed

--- server/routes/socketHandler.js
+++ server/routes/socketHandler.js
@@ -2,29 +2,28 @@ const { ChatMessage } = require('../db/models');
 
 const handleSocketConnection = async (io) => {
   io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+    console.log('User connected:', socket.id);
 
     socket.on('join_room', (roomId) => {
       socket.join(roomId);
-      console.log(`User ${socket.id} joined room ${roomId}`, 111111111111);
+      console.log(`User ${socket.id}: joined room ${roomId}`);
     });
 
     socket.on('send_message', async (data) => {
       const { chatId, authorId, message } = data;
       if (chatId && authorId && message) {
         const chatMessage = await ChatMessage.create({ chatId, authorId, message });
-        // socket.broadcast.emit('recieve_message', { text: data.message, author: 'roommate' });
         io.to(chatId).emit('receive_message', { text: message, author: authorId });
       }
     });
 
     socket.on('leave_room', (roomId) => {
       socket.leave(roomId);
-      console.log(`User ${socket.id} left room ${roomId}`, 2222222222);
+      console.log(`User ${socket.id}: left room ${roomId}`);
     });
 
     socket.on('disconnect', () => {
-      console.log(`User ${socket.id} disconnected`);
+      console.log(`User ${socket.id}: disconnected`);
     });
   });
 };
