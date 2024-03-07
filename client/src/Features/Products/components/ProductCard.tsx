172a074abedd123c9a22b9c152a5b528f049149d
Dianaanaas <nasibullina.di@yandex.ru>
2024-03-07 10:35:38 +0300

    fix chek user

--- client/src/Features/Products/components/ProductCard.tsx
+++ client/src/Features/Products/components/ProductCard.tsx
@@ -9,6 +9,7 @@ import { addMatch } from '../matchSlice'
 import { delProd, dislikeProduct } from '../productSlice'
 import { delFavProd, newFavoriteProduct } from '../../Favorite/favoriteSlice'
 import SwiperComponent from '../../Swiper/Components/Swiper'
+import RegLog from '../../Auth/components/RegLog'
 
 function ProductCard ({ product }: { product: Product }): JSX.Element {
   const dispatch = useAppDispatch()
@@ -20,6 +21,13 @@ function ProductCard ({ product }: { product: Product }): JSX.Element {
   const [sellerRate, setSellerRate] = useState('')
   const [currentProduct, setCurrentProduct] = useState('')
   const [modal, setModal] = useState(false)
+  const [modalReg, setModalReg] = useState(false)
+
+  const modalChange = (): void => {
+    if (user === null) {
+      setModalReg(true)
+    }
+  }
 
   useEffect(() => {
     if (user !== null) {
@@ -46,12 +54,14 @@ function ProductCard ({ product }: { product: Product }): JSX.Element {
   const matchPost = (): void => {
     const userProduct = userProducts.find((product) => product.title === currentProduct)
 
-    if (userProduct !== undefined) {
+    if (userProduct !== undefined && user?.defaultProduct !== 'Добавьте продукт') {
       dispatch(delProd(product.id))
       dispatch(delFavProd(product.id))
       const id = userProduct.id
       dispatch(addMatch({ productId1: id, productId2: product.id }))
         .catch(console.log)
+    } else {
+      navigate('newproduct')
     }
   }
   const newFavorite = (): void => {
@@ -72,6 +82,10 @@ function ProductCard ({ product }: { product: Product }): JSX.Element {
 
   return (
     <>
+                <Modal ariaHideApp={false} className='modal' isOpen={modalReg} onRequestClose={() => { setModalReg(false) }}>
+                  <button onClick={() => { setModalReg(false) }}>x</button>
+                   <RegLog/>
+                </Modal>
 
     <Modal ariaHideApp={false} className='selector-modal' isOpen={modal} onRequestClose={() => { setModal(false) }}>
       <button className='selector-button' onClick={() => { setModal(false) }}>x</button>
@@ -94,9 +108,18 @@ function ProductCard ({ product }: { product: Product }): JSX.Element {
 
     </div>}
 
-      <button type='button' className='product-card__button product-card__fav-button' onClick={newFavorite}>Fav</button>
-      <button type='button' className='product-card__button product-card__like-button' onClick={matchPost}>Like</button>
-      <button type='button' onClick={dislike} className='product-card__button product-card__dislike-button'>Dislike</button>
+      <button type='button' className='product-card__button product-card__fav-button' onClick={() => {
+        newFavorite()
+        modalChange()
+      }}>Fav</button>
+      <button type='button' className='product-card__button product-card__like-button' onClick={() => {
+        matchPost()
+        modalChange()
+      }}>Like</button>
+      <button type='button' onClick={() => {
+        dislike()
+        modalChange()
+      }} className='product-card__button product-card__dislike-button'>Dislike</button>
 
       <div className="seller">
         <div className="seller-container">
